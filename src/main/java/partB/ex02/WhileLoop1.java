6f6d1df52b44211d198ef02eb4fba9c6e0a5ec3e
Lexus Adjei <lexus.adjei@cdpipelinedevshop.com>
2025-03-10 16:56:59 -0400

    update
    horrible update

--- src/main/java/partB/ex02/WhileLoop1.java
+++ src/main/java/partB/ex02/WhileLoop1.java
@@ -1,16 +1,26 @@
 package partB.ex02;
 
+
+
 public class WhileLoop1 {
-    public static String whileLoopOne(){
+    public static String whileLoopOne() {
         String response = "";
 
+        int i = 1, c = 1000;
 
+        do {
+            if (i % 2 == 0){System.out.println(i + " is even ");
+        }
+        i++;
+    } while (i <= c);
 
         return response;
+
     }
 
     public static void main(String[] args) {
         String outputWhileLoopOne = whileLoopOne();
         System.out.println(outputWhileLoopOne);
+        System.out.println("We're done");
     }
 }
--- src/main/java/partB/ex02/WhileLoop2.java
+++ src/main/java/partB/ex02/WhileLoop2.java
@@ -1,9 +1,18 @@
 package partB.ex02;
+import java.util.Scanner;
 
 public class WhileLoop2 {
     public static String whileLoopTwo(){
         String response = "";
+        System.out.println("Please enter a number to count to: ");
+        Scanner in = new Scanner(System.in);
+        in.nextInt();
 
+        int number = Integer.parseInt(in.nextLine());
+
+        while(in <= number){
+            System.out.println("The Current Number is: " + number);
+        }
 
 
         return response;
